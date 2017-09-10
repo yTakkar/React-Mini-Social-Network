1140bf727be39a4c0dd6f9482671946cc2bda4d1
yTakkar <www.shtakkar@gmail.com>
2017-09-11 00:05:49 +0530

    Removed spawn code

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-( () => {
-  var childProcess = require("child_process")
-  var oldSpawn = childProcess.spawn
-  mySpawn = () => {
-    console.log('spawn called')
-    console.log(arguments)
-    var result = oldSpawn.apply(this, arguments)
-    return result
-  }
-  childProcess.spawn = mySpawn
-})()
-
 require('dotenv').config()
 
 // Installed packages
